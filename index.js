0497c6175b6eb3a226787a586a7eb9ad7d57a7f2
parmod <parmodmagotra6391@gmail.com>
2023-08-25 17:27:36 +0530

    added animation in feature-images

--- index.js
+++ index.js
@@ -1,16 +1,51 @@
 const navBar = document.querySelector(".nav-bar");
 const wrapper = document.querySelector("#wrapper");
+const aboutSection = document.querySelector(".about-section");
+const allSkillImages = document.querySelectorAll(".skill-img");
 
 // IMPLEMENTED NAVBAR STICKY POSITION ON SCROLL
-const options = {
+const navOptions = {
   root: null,
   threshold: 0.25,
 };
 
-const callback = () => {
-  navBar.classList.add("sticky-nav");
+function navCallback(entries, target) {
+  const [entry] = entries;
+  if (entry.isIntersecting) {
+    navBar.classList.add("sticky-nav");
+  }
+}
+
+// OBSERVER
+const navObserver = new IntersectionObserver(navCallback, navOptions);
+
+navObserver.observe(aboutSection);
+
+//
+
+// IMPLEMENTED PROJECT-IMAGES ANIMATION ON SCROLLING
+const skillImageOptions = {
+  root: null,
+  threshold: 0.5,
 };
 
-let observer = new IntersectionObserver(callback, options);
+const skillImageCallback = (entries) => {
+  const [entry] = entries;
+  //   EARLY RETURN
+  if (!entry.isIntersecting) return;
 
-observer.observe(wrapper);
+  if (entry.isIntersecting) {
+    allSkillImages.forEach((image) => {
+      image.classList.add("img-animation");
+    });
+  }
+  skillImageObserver.unobserve(entry.target);
+};
+
+// OBSERVER
+const skillImageObserver = new IntersectionObserver(
+  skillImageCallback,
+  skillImageOptions
+);
+
+skillImageObserver.observe(aboutSection);
